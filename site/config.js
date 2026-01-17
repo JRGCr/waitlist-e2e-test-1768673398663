34816199bfc2c6f03dbde5a19ac493d73b76c503
JRC <69986863+JRGCr@users.noreply.github.com>
2026-01-17 14:29:20 -0500

    Add site/config.js

---
+++ site/config.js
@@ -0,0 +1,11 @@
+window.WAITLIST_CONFIG = {
+  "headline": "🚀 Join Our Amazing Product Waitlist!",
+  "description": "Be the first to experience our revolutionary product. Get exclusive early access!",
+  "collect_name": true,
+  "collect_company": false,
+  "primary_color": "#3b82f6",
+  "logo_url": "",
+  "turnstile_enabled": false,
+  "turnstile_site_key": "",
+  "api_url": "WORKER_URL_PLACEHOLDER"
+};
